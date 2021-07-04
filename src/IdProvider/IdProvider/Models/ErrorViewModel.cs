a1688377490dd4f852c3d6b48b41342c0bd153e4
Rune T. Larsen <rune@hjemmekino.no>
2021-07-04 13:59:54 +0200

    First commit

---
+++ src/IdProvider/IdProvider/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IdProvider.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

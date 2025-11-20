0e8fbe63b25ae774b4ed3739e7f094359479fd27
Aleksander Heintz <alxandr@alxandr.me>
2025-11-20 09:38:12 +0100

    chore: service-defaults

--- src/IdProvider/IdProvider/Program.cs
+++ src/IdProvider/IdProvider/Program.cs
@@ -1,32 +1,54 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Altinn.Authorization.ServiceDefaults;
+using IdProvider.Configuration;
+using IdProvider.Services;
+using IdProvider.Services.Implementation;
+using IdProvider.Services.Interface;
+using IdProvider.Services.Interfaces;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Azure.Identity;
 
-namespace IdProvider
+var builder = AltinnHost.CreateWebApplicationBuilder("idprovider", args);
+var services = builder.Services;
+
+services.AddOptions<GeneralSettings>()
+    .BindConfiguration("GeneralSettings")
+    .ValidateOnStart();
+
+services.AddOptions<KeyVaultSettings>()
+    .BindConfiguration("kvSetting")
+    .ValidateOnStart();
+
+services.AddControllersWithViews();
+
+services.AddSingleton<IJwtSigningCertificateProvider, JwtSigningCertificateProvider>();
+services.AddSingleton<IToken, TokenService>();
+services.AddLogging();
+services.AddApplicationInsightsTelemetry(opts =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    opts.ConnectionString = builder.Configuration["APPINSIGHTS_CONNECTIONSTRING"];
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-        .ConfigureLogging(logging =>
-        {
-            logging.ClearProviders();
-            logging.AddConsole();
-        })
-        .ConfigureWebHostDefaults(webBuilder =>
-        {
-            webBuilder.UseStartup<Startup>();
-        });
-    }
+var app = builder.Build();
+var env = app.Environment;
+
+if (env.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
